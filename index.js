f08d0fc95a4425b0dc51ff05254f83806506cb65
Daniel Ketterer <danielnorton0295@gmail.com>
2024-02-07 16:55:10 -0500

    created portfolio landing page

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var headshot = document.querySelector('.headshot');
+    var originalSrc = headshot.src;
+
+    headshot.addEventListener("mouseenter", function () {
+        headshot.style.opacity = 0; // Fade out
+        setTimeout(function () {
+            headshot.src = "assets/greyDan.png"; // Change image source
+            headshot.style.opacity = 1; // Fade in
+        }, 200);
+    });
+
+    headshot.addEventListener("mouseleave", function () {
+        headshot.style.opacity = 0; // Fade out
+        setTimeout(function () {
+            headshot.src = originalSrc; // Restore original image source
+            headshot.style.opacity = 1; // Fade in
+        }, 200);
+    });
+});
+
